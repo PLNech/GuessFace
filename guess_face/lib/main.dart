464c3f8e80b6689c94d3ce502eb1b3b381917ed8
Paul-Louis NECH <pln@algolia.com>
2019-04-05 16:04:03 +0200

    game: Main display

--- guess_face/lib/main.dart
+++ guess_face/lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:math';
+
 import 'package:algolia/algolia.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
+
+const imageWidth = 200;
 
 class MyApp extends StatelessWidget {
   static final Algolia _algolia = Algolia.init(
@@ -9,24 +14,43 @@ class MyApp extends StatelessWidget {
     apiKey: '0f7fbe1c34e2e1ade92120bb6b221d06',
   );
 
-  static Future<AlgoliaQuerySnapshot> _loadAlgolians() async {
+  static Future<GuessData> _loadAlgolians() async {
+    debugPrint("Loading Algolians!");
     AlgoliaQuerySnapshot snap = await _algolia.instance
         .index("algolia.com-about-page")
         .search("")
         .setHitsPerPage(1000)
         .getObjects();
+    debugPrint("Got hits: ${snap.hits.length}");
 
-    print("Got hits: ${snap.hits.length}");
-    return snap;
+    var algolians = snap.hits..shuffle();
+
+    // Select an Algolian with valid image
+    for (var algolian in algolians) {
+      // "https://www.gravatar.com/avatar/7f6d9be8b64f32120bd6ef07e3b3b501?d=404";
+      var gravatarURL = algolian.data["gravatar"] + "?s=$imageWidth&d=404";
+      if ((await http.get(gravatarURL)).statusCode == 200) {
+        algolians.remove(algolian);
+        var options = algolians.sublist(0, 3);
+        options.add(algolian);
+        return GuessData(
+            gravatarURL,
+            algolian,
+            options.map((it) => (it.data["name"] as String)).toList()
+              ..shuffle());
+      }
+      debugPrint("Algolian ${algolian.data["name"]} has invalid picture, next");
+    }
+    throw new Exception("No algolian has valid image!!1!");
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'GuessFace',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Guess an Algolian 🙈'),
         ),
         body: Center(child: GuessWidget()),
       ),
@@ -42,14 +66,24 @@ class GuessWidget extends StatefulWidget {
 class GuessState extends State<GuessWidget> {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<AlgoliaQuerySnapshot>(
+    return FutureBuilder<GuessData>(
         future: MyApp._loadAlgolians(),
-        builder: (context, snapshot) {
+        builder: (context, guessData) {
+          if (guessData.hasError) {
+            debugPrint("ERROR! ${guessData.error}");
+          }
+          var data = guessData.data;
+          debugPrint("Ready to build list for data: $data");
           return Padding(
               padding: EdgeInsets.all(16.0),
               child: Column(children: <Widget>[
-                Text('Loaded ${snapshot.data.nbHits} algolians!'),
-                Expanded(child: _buildSuggestionList(snapshot))
+                Image.network(data.imageURL,
+                    width: imageWidth.toDouble(), fit: BoxFit.fill),
+                Padding(padding: EdgeInsets.symmetric(vertical: 10.0)),
+                Text('Guess the name 🧐',
+                    style: Theme.of(context).textTheme.headline),
+                Padding(padding: EdgeInsets.symmetric(vertical: 10.0)),
+                Expanded(child: _buildSuggestionList(data.options))
               ]));
         });
     /**
@@ -59,16 +93,32 @@ class GuessState extends State<GuessWidget> {
      */
   }
 
-  _buildSuggestionList(AsyncSnapshot<AlgoliaQuerySnapshot> objects) {
+  _buildSuggestionList(List<String> objects) {
     return ListView.builder(
+      itemCount: objects.length,
       padding: const EdgeInsets.all(16.0),
       itemBuilder: (context, i) {
-        return _buildRow(objects.data.hits[i].data);
+        return _buildRow(objects[min(i, objects.length - 1)]);
       },
     );
   }
 
-  _buildRow(Map<String, dynamic> hit) {
-    return ListTile(title: Text(hit["name"]));
+  _buildRow(String name) {
+    return ListTile(
+        title: Text(name,
+            style: Theme.of(context).textTheme.display1));
   }
+}
+
+class GuessData {
+  final String imageURL;
+  final AlgoliaObjectSnapshot guessMe;
+  final List<dynamic> options;
+
+  @override
+  String toString() {
+    return "${guessMe.data["name"]}, $imageURL, ${options.toString()}";
+  }
+
+  GuessData(this.imageURL, this.guessMe, this.options);
 }
